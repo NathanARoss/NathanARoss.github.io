eded89e3df251805cf7ad7b17134058b5d6a37fe
NathanAnthonyRoss19 <NathanAnthonyRoss19@gmail.com>
2018-01-25 23:03:40 -0800

    Updated console layout test

--- console_layout_test/js/incremental_logic.js
+++ console_layout_test/js/incremental_logic.js
@@ -1,24 +1,9 @@
 var answer = 0
 
 function entryPoint() {
-	startNewGame();
+	sendOutput("Console Output Test");
 }
 
 function processInput(input) {
-	
-	let guess = parseInt(input);
-	
-	if (guess < answer) {
-		sendOutput(guess + " is too low");
-	} else if (guess > answer) {
-		sendOutput(guess + " is too high");
-	} else {
-		sendOutput("ding ding ding! The answer is " + answer + "<br>");
-		startNewGame();
-	}
-}
-
-function startNewGame() {
-	sendOutput("Guess my integer between 1 and 100 inclusive");
-	answer = Math.floor(Math.random() * 100) + 1;
+	sendOutput(input);
 }
