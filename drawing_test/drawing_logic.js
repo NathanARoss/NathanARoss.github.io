52e19c7390eab52190aaecf536742c8a0f001c91
NathanAnthonyRoss19 <NathanAnthonyRoss19@gmail.com>
2018-01-29 00:39:41 -0800

    added canvas drawing demo

---
+++ drawing_test/drawing_logic.js
@@ -0,0 +1,36 @@
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+
+var theta = 0.0;
+var centerX, centerY;
+var time = 0.0;
+
+function resize_canvas() {
+	context.canvas.width = window.innerWidth;
+	context.canvas.height = window.innerHeight;
+	centerX = canvas.width / 2.0;
+	centerY = canvas.height / 2.0;
+}
+
+function draw() {
+	//clear canvas
+	//context.beginPath();
+	//context.clearRect(0, 0, canvas.width, canvas.height);
+	
+	let radius = Math.sin(time * 0.0201) * (canvas.width / 3.0);
+	var x = Math.floor(Math.cos(theta) * radius + centerX);
+	var y = Math.floor(Math.sin(theta) * radius + centerY);
+	context.moveTo(x,y);
+	
+	theta += 0.1;
+	time += 1.0;
+	
+	radius = Math.sin(time * 0.0201) * (canvas.width / 3.0);
+	x = Math.floor(Math.cos(theta) * radius + centerX);
+	y = Math.floor(Math.sin(theta) * radius + centerY);
+	context.lineTo(x,y);
+	context.stroke();
+}
+
+resize_canvas();
+setInterval(draw, 33);
