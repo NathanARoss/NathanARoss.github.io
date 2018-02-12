81fd0081464646199b5d26ccdfae31ac59a34362
NathanAnthonyRoss19 <NathanAnthonyRoss19@gmail.com>
2018-02-11 23:36:29 -0800

    Updated to better handle resizing and to allow jumping through the page to not simulate a large amount of scrolling.
    Dragging the scroll wheel from the top of the page to the bottom now generates the list from scratch instead of pretending that the user scrolled down there themselves.

--- progressive_list/scrolling_logic.js
+++ progressive_list/scrolling_logic.js
@@ -1,9 +1,10 @@
 var screenHeight;
 var itemPoolSize = 0;
 var visibleItemCount = 0;
-let itemHeight = 60;
+let itemHeight = 30;
 var firstLoadedItemIndex = 0;
-let itemCount = 10000;
+var firstVisibleItemIndex = 0;
+let itemCount = Math.floor(2**24 / itemHeight);
 
 var list = document.getElementById("list"); //a div containing all elements
 var spacer = document.getElementById("spacer"); //a div that changes size to offset elements
@@ -20,58 +21,98 @@ function resizeListener() {
 	
 	if (diff > 0) {
 		for(var i = 0; i < diff; ++i) {
-			var p = document.createElement("p");
-			p.classList.add("item");
-			
-			p.appendChild(document.createTextNode(i));
-			//p.appendChild(document.createElement("br"));
-			//p.appendChild(document.createTextNode(i));
-			
 			var div = document.createElement("div");
 			div.classList.add("item");
-
-			div.appendChild(p);
-			list.appendChild(div);
+			
+			let position = list.childNodes.length + firstLoadedItemIndex;
+			
+			//if the user is scrolled all the way to the bottom, prepend instead of appending
+			if (position <= itemCount) {
+				list.insertBefore(div, list.firstChild);
+				appendItem(position);
+			} else {
+				list.append(div);
+				prependItem(firstLoadedItemIndex - 1);
+				--firstLoadedItemIndex;
+				spacer.style.height = firstLoadedItemIndex * itemHeight + "px";
+			}
 		}
 	} else if (diff < 0) {
 		diff = -diff;
-		let length = list.childNodes.length;
 		for (var i = 0; i < diff; ++i) {
-			var pos = length - 1 - i;
-			console.log("removing child at " + pos);
-			var child = list.childNodes[pos];
-			list.removeChild(child);
+			let lastChild = list.childNodes[list.childNodes.length - 1];
+			list.removeChild(lastChild);
 		}
 	}
+	
+	firstVisibleItemIndex = Math.floor(window.scrollY / itemHeight);
+	updateDebug();
 }
 
 window.onscroll = function() {
-	let firstVisibleItemIndex = Math.floor(window.scrollY / itemHeight);
+	firstVisibleItemIndex = Math.floor(window.scrollY / itemHeight);
+	
+	//user scrolled so far down all items are off screen
+	if (firstVisibleItemIndex >= firstLoadedItemIndex + itemPoolSize) {
+		//set the first loaded item far above the viewport so the following loop refills the screen
+		firstLoadedItemIndex = firstVisibleItemIndex - itemPoolSize - 2;
+		
+		let position = firstLoadedItemIndex + itemPoolSize;
+		list.childNodes[list.childNodes.length - 1].innerHTML = position;
+	}
+	
+	//user scrolled so far up all items are off screen
+	if (firstVisibleItemIndex <= firstLoadedItemIndex - itemPoolSize) {
+		//set the first loaded item far below the viewport so the following loop refills the screen
+		firstLoadedItemIndex = firstVisibleItemIndex + itemPoolSize;
+		
+		let position = firstLoadedItemIndex;
+		list.firstChild.innerHTML = position;
+	}
 	
 	//keep a buffer of 2 unseen elements in either direction
 	while ((firstVisibleItemIndex - 4 > firstLoadedItemIndex) && (firstLoadedItemIndex < itemCount - itemPoolSize)) {
-		var firstChild = list.firstChild;
-		firstChild.innerHTML = (itemPoolSize + firstLoadedItemIndex);
-		list.removeChild(firstChild);
-		list.appendChild(firstChild);
+		appendItem(itemPoolSize + firstLoadedItemIndex);
 		++firstLoadedItemIndex;
-		
-		spacer.style.height = firstLoadedItemIndex * itemHeight + "px";
 	}
 	
 	while ((firstVisibleItemIndex - 2 < firstLoadedItemIndex) && (firstLoadedItemIndex > 0)) {
-		var lastChild = list.childNodes[list.childNodes.length - 1];
-		lastChild.innerHTML = (firstLoadedItemIndex - 1);
-		list.removeChild(lastChild);
-		list.insertBefore(lastChild, list.firstChild);
+		prependItem(firstLoadedItemIndex - 1);
 		--firstLoadedItemIndex;
-		
-		spacer.style.height = firstLoadedItemIndex * itemHeight + "px";
 	}
 	
+	spacer.style.height = firstLoadedItemIndex * itemHeight + "px";
+	updateDebug();
+}
+
+function appendItem(position) {
+	position = position|0;
+	
+	var firstChild = list.firstChild;
+	firstChild.innerHTML = fancyElementFunction(position);
+	list.removeChild(firstChild);
+	list.appendChild(firstChild);
+}
+
+function prependItem(position) {
+	position = position|0;
+	
+	var lastChild = list.childNodes[list.childNodes.length - 1];
+	lastChild.innerHTML = fancyElementFunction(position);
+	list.removeChild(lastChild);
+	list.insertBefore(lastChild, list.firstChild);
+}
+
+function fancyElementFunction(position) {
+	position = position|0;
+	
+	return position;
+}
+
+function updateDebug() {
 	var debugText = "scrollY: " + window.scrollY + "<br>"
-					+ "loaded items: [" + firstLoadedItemIndex + ", " + (firstLoadedItemIndex + itemPoolSize - 1) + "]<br>"
-					+ "visible items: [" + firstVisibleItemIndex + ", " + (firstVisibleItemIndex + visibleItemCount - 1) + "]";
+			+ "loaded items: [" + firstLoadedItemIndex + ", " + (firstLoadedItemIndex + itemPoolSize - 1) + "]<br>"
+			+ "visible items: [" + firstVisibleItemIndex + ", " + (firstVisibleItemIndex + visibleItemCount - 1) + "]";
 	debug.innerHTML = debugText;
 }
 
